2595d6faae16cf2c8034d9a88c2619ed5f96d04a
Ondrej Janus <ojanus@redhat.com>
2022-09-14 13:15:55 +0200

    tmp

--- spec-tests/src/test/java/org/candlepin/spec/imports/ImportErrorSpecTest.java
+++ spec-tests/src/test/java/org/candlepin/spec/imports/ImportErrorSpecTest.java
@@ -16,6 +16,7 @@
 package org.candlepin.spec.imports;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.candlepin.spec.bootstrap.assertions.StatusCodeAssertions.assertConflict;
 
 import org.candlepin.dto.api.client.v1.AsyncJobStatusDTO;
@@ -43,6 +44,7 @@ import org.junit.jupiter.api.parallel.ExecutionMode;
 import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.List;
 
 public class ImportErrorSpecTest {
@@ -101,6 +103,134 @@ public class ImportErrorSpecTest {
             .containsOnly("Import is the same as existing data");
     }
 
+    @Test
+    void shouldAllowForcingTheSameManifest() {
+        assertThatNoException()
+            .isThrownBy(() -> importNow(owner.getKey(), importFile, "MANIFEST_SAME", "DISTRIBUTOR_CONFLICT"));
+    }
+
+    @Test
+    void shouldAllowImportingOlderManifestsIntoAnotherOwner() {
+        // TODO use old import
+        assertThatNoException()
+            .isThrownBy(() -> importNow(owner.getKey(), importFile, "MANIFEST_SAME", "DISTRIBUTOR_CONFLICT"));
+    }
+
+    @Test
+    void shouldReturnConflictWhenImportingManifestFromDifferentSubscriptionManagementApplication() throws ApiException {
+        OwnerDTO otherOrg = admin.owners().createOwner(Owners.random());
+
+        assertConflict(() -> importNow(otherOrg.getKey(), importFile))
+            .hasMessageContaining("DISTRIBUTOR_CONFLICT");
+
+        assertThat(owner.getUpstreamConsumer().getUuid()).isEqualTo(IMPORT_CONSUMER_UUID);
+
+        assertConflict(() -> importNow(otherOrg.getKey(), importFile))
+            .hasMessageNotContaining("MANIFEST_SAME")
+            .hasMessageContaining("DISTRIBUTOR_CONFLICT");
+    }
+
+//       # TODO
+//    it 'should allow forcing a manifest from a different subscription management application' do
+//    exporter = StandardExporter.new
+//    @exporters << exporter
+//        another = exporter.create_candlepin_export().export_filename
+//
+//    old_upstream_uuid = @cp.get_owner(@import_owner['key'])['upstreamConsumer']['uuid']
+//    pools = @cp.list_owner_pools(@import_owner['key'])
+//    pool_ids = pools.collect { |p| p['id'] }
+//    @import_method.call(@import_owner['key'], another,
+//        {:force => ['DISTRIBUTOR_CONFLICT']})
+//    @cp.get_owner(@import_owner['key'])['upstreamConsumer']['uuid'].should_not == old_upstream_uuid
+//        pools = @cp.list_owner_pools(@import_owner['key'])
+//        new_pool_ids = pools.collect { |p| p['id'] }
+//      # compare without considering order, pools should have changed completely:
+//    new_pool_ids.should_not =~ pool_ids
+//        end
+    @Test
+    void shouldAllowForcingManifestFromDifferentSubscriptionManagementApplication() throws ApiException {
+        OwnerDTO otherOrg = admin.owners().createOwner(Owners.random());
+
+        assertConflict(() -> importNow(otherOrg.getKey(), importFile))
+            .hasMessageContaining("DISTRIBUTOR_CONFLICT");
+
+        assertThat(owner.getUpstreamConsumer().getUuid()).isEqualTo(IMPORT_CONSUMER_UUID);
+
+        assertConflict(() -> importNow(otherOrg.getKey(), importFile))
+            .hasMessageNotContaining("MANIFEST_SAME")
+            .hasMessageContaining("DISTRIBUTOR_CONFLICT");
+    }
+
+       # TODO
+    it 'should allow forcing a manifest from a different subscription management application' do
+    exporter = StandardExporter.new
+    @exporters << exporter
+        another = exporter.create_candlepin_export().export_filename
+
+    old_upstream_uuid = @cp.get_owner(@import_owner['key'])['upstreamConsumer']['uuid']
+    pools = @cp.list_owner_pools(@import_owner['key'])
+    pool_ids = pools.collect { |p| p['id'] }
+    @import_method.call(@import_owner['key'], another,
+        {:force => ['DISTRIBUTOR_CONFLICT']})
+    @cp.get_owner(@import_owner['key'])['upstreamConsumer']['uuid'].should_not == old_upstream_uuid
+        pools = @cp.list_owner_pools(@import_owner['key'])
+        new_pool_ids = pools.collect { |p| p['id'] }
+      # compare without considering order, pools should have changed completely:
+    new_pool_ids.should_not =~ pool_ids
+        end
+
+//    # TODO
+//    it 'should return 400 when importing manifest in use by another owner' do
+//        # Because the previous tests put the original import into a different state
+//      # than if you just run this single one, we need to clear first and then
+//      # re-import the original.
+//        # Also added the confirmation that the exception occurs when importing to
+//      # another owner.
+//    job = @import_owner_client.undo_import(@import_owner['key'])
+//    wait_for_job(job['id'], 30)
+//
+//    @import_method.call(@import_owner['key'], @cp_export_file)
+//    owner2 = @cp.create_owner(random_string("owner2"))
+//    exception = false
+//    expected = "This subscription management application has already been imported by another owner."
+//    begin
+//    @import_method.call(owner2['key'], @cp_export_file)
+//    rescue RestClient::Exception => e
+//    async.should be false
+//    e.http_code.should == 400
+//    json = JSON.parse(e.http_body)
+//    exception = true
+//    rescue AsyncImportFailure => aif
+//    async.should be true
+//    json = aif.data["resultData"]
+//    json.should_not be_nil
+//    exception = true
+//    end
+//
+//    @cp.delete_owner(owner2['key'])
+//    exception.should be true
+//        if async
+//    json.include?(expected).should be true
+//        else
+//    message = json["displayMessage"]
+//    message.should_not be_nil
+//    message.should == expected
+//        end
+//    end
+    @Test
+    void sasdhouldAllowForcingManifestFromDifferentSubscriptionManagementApplication() throws ApiException {
+        OwnerDTO otherOrg = admin.owners().createOwner(Owners.random());
+
+        assertConflict(() -> importNow(otherOrg.getKey(), importFile))
+            .hasMessageContaining("DISTRIBUTOR_CONFLICT");
+
+        assertThat(owner.getUpstreamConsumer().getUuid()).isEqualTo(IMPORT_CONSUMER_UUID);
+
+        assertConflict(() -> importNow(otherOrg.getKey(), importFile))
+            .hasMessageNotContaining("MANIFEST_SAME")
+            .hasMessageContaining("DISTRIBUTOR_CONFLICT");
+    }
+
     private static void undoImport(OwnerDTO owner) throws ApiException {
         AsyncJobStatusDTO job = admin.owners().undoImports(owner.getKey());
         admin.jobs().waitForJob(job);
@@ -125,6 +255,10 @@ public class ImportErrorSpecTest {
 
     private static void importNow(String ownerKey, File export) throws ApiException {
         admin.owners().importManifest(ownerKey, List.of("DISTRIBUTOR_CONFLICT"), export);
+    }
+
+    private static void importNow(String ownerKey, File export, String... force) throws ApiException {
+        admin.owners().importManifest(ownerKey, Arrays.asList(force), export);
     }
 
     private void importAsync(String ownerKey, File export) throws ApiException {
