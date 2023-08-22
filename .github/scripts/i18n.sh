1cb22471cbfe56fecda244bb646b333ba6493c0f
William Poteat <wpoteat@redhat.com>
2023-08-22 08:19:58 -0400

    new branch

--- .github/scripts/i18n.sh
+++ .github/scripts/i18n.sh
@@ -16,13 +16,15 @@ files=$(git diff po/keys.pot | egrep -v -e '^( |\+#|\-#|@@|\+\+\+|\-\-\-|diff|in
 
 if [ ! -z "$files" ]; then
   #Code to commit the updated template file
+  NEW_BRANCH=i18n/$(date '+%s')
+  git checkout -b $NEW_BRANCH
   git add po/keys.pot
-  evalrc $? " <== System return for git add file for branch $GIT_BRANCH."
+  evalrc $? " <== System return for git add file for branch $NEW_BRANCH."
 
   echo "Committing and pushing the template file."
   git -c "user.name=$GIT_AUTHOR_NAME" -c "user.email=$GIT_AUTHOR_EMAIL" commit -m "updated po/keys.pot template"
-  evalrc $? " <== System return for git commit for branch $GIT_BRANCH."
+  evalrc $? " <== System return for git commit for branch $NEW_BRANCH."
 
-  git push https://candlepin-bot:${I18N_TOKEN}@github.com/candlepin/candlepin $GIT_BRANCH
-  evalrc $? " <== System return for git push for branch $GIT_BRANCH."
+  git push https://candlepin-bot:${I18N_TOKEN}@github.com/candlepin/candlepin $NEW_BRANCH
+  evalrc $? " <== System return for git push for branch $NEW_BRANCH."
 fi
